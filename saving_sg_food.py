d1bf680dbe993be9d3f2bd372383ede0ab98334d
gokaijh <junhan-@hotmail.com>
2019-01-20 11:42:34 +0800

    upda

--- saving_sg_food.py
+++ saving_sg_food.py
@@ -8,6 +8,8 @@ import time
 import json
 import requests
 
+import getLastMessage
+
 from telegram import message
 import sys
 from get_new_query import DBQuery
@@ -22,6 +24,92 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+
+TOKEN = "662276798:AAFbFHPtT9I7_sNzKAjKc14XW-b-ZCLT7TU"
+URL = "https://api.telegram.org/bot{}/".format(TOKEN)
+
+
+def get_url(url):
+    response = requests.get(url)
+    content = response.content.decode("utf8")
+    return content
+
+
+def get_json_from_url(url):
+    content = get_url(url)
+    js = json.loads(content)
+    return js
+
+
+def get_updates(offset=None):
+    url = URL + "getUpdates?timeout=100"
+    if offset:
+        url += "&offset={}".format(offset)
+    js = get_json_from_url(url)
+    return js
+
+
+def get_last_update_id(updates):
+    update_ids = []
+    for update in updates["result"]:
+        update_ids.append(int(update["update_id"]))
+    return max(update_ids)
+
+
+def echo(bot, update):
+    text = update.message.text
+    chat = update.message.chat_id
+    getvendors(text, chat)
+
+def echo_all(updates):
+    for update in updates["result"]:
+        try:
+            text = update["message"]["text"]
+            chat = update["message"]["chat"]["id"]
+            print(text)
+
+        except Exception as e:
+            print(e)
+        # send_message(text, chat)
+
+
+def get_last_chat_id_and_text(updates):
+    num_updates = len(updates["result"])
+    last_update = num_updates - 1
+    text = updates["result"][last_update]["message"]["text"]
+    chat_id = updates["result"][last_update]["message"]["chat"]["id"]
+    return text, chat_id
+
+
+
+"""
+def send_message(text, chat_id):
+    text = urllib.quote_plus(text)
+    url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
+    get_url(url)
+"""
+
+
+def getvendors(text, chat_id):
+    r = requests.get('http://127.0.0.1:5000/foods/' + text + '/vendors.json')
+    g = r.json()
+    tgt = []
+    for h in g:
+        tgt.append(h)
+
+    url = URL + "sendMessage?text={}&chat_id={}".format(tgt, chat_id)
+    get_url(url)
+
+
+def retrieve_message():
+    last_update_id = None
+    while True:
+        updates = get_updates(last_update_id)
+        if "result" in updates:
+            last_update_id = get_last_update_id(updates) + 1
+            echo_all(updates)
+        time.sleep(1)
 
 
 def start(bot, update):
@@ -56,7 +144,6 @@ def find_by_food(bot, update):
     tgt = []
     for h in g:
         tgt.append(h)
-
     update.message.reply_text(str(h['id']) + " - " + h['name'])
 
 
@@ -81,8 +168,7 @@ def main():
     dp.add_handler(CommandHandler("Find_by_Food", find_by_food))
     dp.add_handler(CommandHandler("searchFood", search_by_name))
     dp.add_handler(MessageHandler(Filters.location, location))
-
-
+    dp.add_handler(MessageHandler(Filters.text, echo))
     # log all errors
     dp.add_error_handler(error)
 
--- testRun.py
+++ testRun.py
@@ -1,7 +1,8 @@
-import time
 import json
 import requests
+import time
 import urllib
+
 
 TOKEN = "662276798:AAFbFHPtT9I7_sNzKAjKc14XW-b-ZCLT7TU"
 URL = "https://api.telegram.org/bot{}/".format(TOKEN)
@@ -20,7 +21,7 @@ def get_json_from_url(url):
 
 
 def get_updates(offset=None):
-    url = URL + "getUpdates"
+    url = URL + "getUpdates?timeout=100"
     if offset:
         url += "?offset={}".format(offset)
     js = get_json_from_url(url)
@@ -38,9 +39,7 @@ def echo_all(updates):
     for update in updates["result"]:
         text = update["message"]["text"]
         chat = update["message"]["chat"]["id"]
-        print(text)
-        getvendors(text, chat)
-        # send_message(text, chat)
+        send_message(text, chat)
 
 
 def get_last_chat_id_and_text(updates):
@@ -48,34 +47,24 @@ def get_last_chat_id_and_text(updates):
     last_update = num_updates - 1
     text = updates["result"][last_update]["message"]["text"]
     chat_id = updates["result"][last_update]["message"]["chat"]["id"]
-    return text, chat_id
+    return (text, chat_id)
 
 
-"""
 def send_message(text, chat_id):
     text = urllib.quote_plus(text)
     url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
     get_url(url)
-"""
 
 
-def getvendors(text, chat_id):
-    r = requests.get('http://127.0.0.1:5000/foods/' + text + '/vendors.json')
-    g = r.json()
-    tgt = []
-    for h in g:
-        tgt.append(h)
-
-    url = URL + "sendMessage?text={}&chat_id={}".format(tgt, chat_id)
-    get_url(url)
-
-
-def retrieve_message():
+def main():
     last_update_id = None
     while True:
         updates = get_updates(last_update_id)
-        if len(updates["result"]) > 0:
+        if "result" in updates:
             last_update_id = get_last_update_id(updates) + 1
             echo_all(updates)
+        time.sleep(0.5)
 
-    time.sleep(0.5)
+
+if __name__ == '__main__':
+    main()
